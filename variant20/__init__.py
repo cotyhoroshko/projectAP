377f182ad1e6ef586dab2d619733a3a631432e90
Viktor Sabov <viktorszabo2002@gmail.com>
2020-11-26 00:22:26 +0200

    did a refactor

---
+++ variant20/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def run_foo():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return 'Вітаю Вас!'
+
+    @app.route('/api/v1/hello-world-20')
+    def hello_world_20():
+        return 'Hello World 20'
+
+    return app
